88743dfc152c91a745503db59287f2e4fe185ec6
elmo <borromeo@momo.so-net.ne.jp>
2023-12-22 11:55:34 +0900

    gallery count on dashboard navbar

--- app/Filament/Resources/GalleryResource.php
+++ app/Filament/Resources/GalleryResource.php
@@ -31,6 +31,10 @@ class GalleryResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
+    public static function getNavigationBadge(): ?string {
+        return static::getModel()::count();
+    }
+
     public static function form(Form $form): Form
     {
         return $form
