a9f4f9633ae81e593e69cad3e116ad9fc864f132
elmo <borromeo@momo.so-net.ne.jp>
2023-12-27 17:40:40 +0900

    preview full functionality

--- resources/views/filament/components/testing.blade.php
+++ resources/views/filament/components/testing.blade.php
@@ -9,13 +9,11 @@
     }
 @endphp
 
-
-{{-- {!! $image->temporaryUrl() !!} --}}
-{{-- <img src="{{ $image->temporaryUrl() }}"> --}}
-<div x-data class="flex flex-row flex-wrap justify-around items-start max-w-screen-lg  mx-auto mt-12">
-  
+<div class="flex flex-row flex-wrap justify-around items-start max-w-screen-lg  mx-auto mt-12">
     @if ($image == "")
-      <p class="text-center">missing image</p>
+    <figure class="mx-auto">
+      <img src="{{ asset('images/no image.png') }}" class="mb-30" />
+    </figure>
     @elseif (is_string($image))
       <figure class="mx-auto">
         <img src="{{ 'https://setup.satomisuzuki.info/storage/'.$image }}" class="mb-30" />
@@ -34,12 +32,51 @@
         <div class="mb-10 text-xl text-gray-700 dark:text-gray-400">
           {!! $data['details-ja'] ? $data['details-ja'] : 'details missing' !!}
         </div>
-        <div class="mt-5 text-left text-xl text-gray-700 dark:text-gray-400">{!! $data['desc-ja'] !!}</div>
+        <div class="mt-5 text-left text-xl text-gray-700 dark:text-gray-400">
+          {!! $data['desc-ja'] !!}
+        </div>
         @if($data['availability'] == 0)
           <p class="text-xl font-bold mt-10 !text-red-600 ">SOLD</p>
         @endif
-        <div class="mb-10"> </div>
+        <div class="mb-10">        
+        </div>
       </div>
     </div>
+</div>
+<hr>
+<div class="flex flex-row flex-wrap justify-around items-start max-w-screen-lg  mx-auto mt-12">
+  @if ($image == "")
+  <figure class="mx-auto">
+    <img src="{{ asset('images/no image.png') }}" class="mb-30" />
+  </figure>
+  @elseif (is_string($image))
+    <figure class="mx-auto">
+      <img src="{{ 'https://setup.satomisuzuki.info/storage/'.$image }}" class="mb-30" />
+    </figure>
+  @else
+    <figure class="mx-auto">
+      <img src="{{ $image->temporaryUrl() }}" />
+    </figure>
+  @endif
+  <div class="flex items-center justify-items-center text-center">
+  {{-- <div class="hero-content text-center text-neutral-content"> --}}
+  <div class="max-w-xl font-[300] text-xl font-en">
+    <h1 class="my-5 leading-[3.75rem] text-4xl font-xl text-gray-500 dark:text-gray-400">
+      {!! $data['title-en'] != '' ? $data['title-en'] : 'title missing' !!}
+    </h1>
+    <div class="mb-10 text-xl text-gray-700 dark:text-gray-400">
+      {!! $data['details-en'] ? $data['details-en'] : 'details missing' !!}
+    </div>
+    <div class="mt-5 text-left text-xl text-gray-700 dark:text-gray-400">
+      {!! $data['desc-en'] !!}
+    </div>
+    @if($data['availability'] == 0)
+      <p class="text-xl font-bold mt-10 !text-red-600 ">SOLD</p>
+    @endif
+    <div class="mb-10">
+      
+    </div>
+  </div>
+</div>
     
 {{-- {{ is_string($image) }} --}}
